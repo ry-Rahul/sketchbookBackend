603e3b3be0b2218b97370574c557ce10d4ff980b
Rahul Yadav <ryrahul12345@gmail.com>
2024-02-21 18:51:33 +0530

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const port = 3005;
+const PORT = 3005;
 const app = express();
 const httpServer = createServer(app);
 
