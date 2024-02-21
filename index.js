3323fb7e02827d1ca3890a921c7013f4d3bea57e
Rahul Yadav <ryrahul12345@gmail.com>
2024-02-21 18:24:50 +0530

    backend setup for sektchbook

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const port = 3005;
+const app = express();
+const httpServer = createServer(app);
+
+app.use(cors({ origin: "http://localhost:3000" }));
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    socket.on("beginPath", (data) => {
+        socket.broadcast.emit("beginPath", data);
+    });
+
+    socket.on("drawLine", (data) => {
+        socket.broadcast.emit("drawLine", data); 
+    });
+});
+
+httpServer.listen(port, () => {
+    console.log(`listening on *:${port}`);
+});
